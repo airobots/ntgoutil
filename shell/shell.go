0ca96c02354aeca9c14b1c91340e7083105e202b
Rony Novianto <rony@novianto.tech>
2019-08-12 04:51:51 -0700

    Add git ignore and run script function

--- shell/shell.go
+++ shell/shell.go
@@ -10,92 +10,100 @@
 package shell
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path"
 )
 
-func CopyFile(src, dest string, specifiedMode ...os.FileMode) error {
+func CopyFile(sourcePath, destinationPath string, uid, gid int, fileMode, directoryMode *os.FileMode) error {
 	var err error
-	var copyMode os.FileMode
-
-	srcFile, err := os.Open(src)
+	sourceFile, err := os.Open(sourcePath)
 	if err != nil {
 		return err
 	}
-	defer srcFile.Close()
-
-	destFile, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer destFile.Close()
-	
-	_, err = io.Copy(destFile, srcFile)
-	if err != nil {
-		return err
-	}
-	
-	if len(specifiedMode) > 0 {
-		copyMode = specifiedMode[0]
+	defer sourceFile.Close()
+	directoryPath := path.Dir(destinationPath)
+	if directoryMode == nil {
+		err = os.MkdirAll(directoryPath, os.ModePerm)
 	} else {
-		info, err := os.Stat(src)
+		err = os.MkdirAll(directoryPath, *directoryMode)
+	}
+	if err != nil {
+		return err
+	}
+	destinationFile, err := os.Create(destinationPath)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+	if uid >= 0 || gid >= 0 {
+		err = destinationFile.Chown(uid, gid)
 		if err != nil {
 			return err
 		}
-		copyMode = info.Mode()
 	}
-	
-	os.Chmod(dest, copyMode)
-	
-	return destFile.Sync()
+	if fileMode != nil {
+		err = destinationFile.Chmod(*fileMode)
+		if err != nil {
+			return err
+		}
+	}
+	return destinationFile.Sync()
 }
 
-func CopyDirectory(sourceDirectory, destinationDirectory string, specifiedMode ...os.FileMode) error {
-	var copyMode os.FileMode
-	
-	infos, err := ioutil.ReadDir(sourceDirectory)
+func Copy(source, destination string, uid, gid int, fileMode, directoryMode *os.FileMode) error {
+	info, err := os.Stat(source)
+	originalMode := info.Mode()
 	if err != nil {
 		return err
 	}
-	
-	if len(specifiedMode) > 0 {
-		copyMode = specifiedMode[0]
-	} else {
-		info, err := os.Stat(sourceDirectory)
+	if !info.IsDir() {
+		if fileMode == nil {
+			err = CopyFile(source, destination, uid, gid, &originalMode, directoryMode)
+		} else {
+			err = CopyFile(source, destination, uid, gid, fileMode, directoryMode)
+		}
 		if err != nil {
 			return err
 		}
-		copyMode = info.Mode()
+		return nil
 	}
-	
-	// Create any missing directory
-	err = os.MkdirAll(destinationDirectory, copyMode)
+	if directoryMode == nil {
+		err = os.MkdirAll(destination, originalMode)
+	} else {
+		err = os.MkdirAll(destination, *directoryMode)
+	}
 	if err != nil {
 		return err
 	}
-	
+	infos, err := ioutil.ReadDir(source)
+	if err != nil {
+		return err
+	}
 	for _, info := range infos {
 		name := info.Name()
-		sourceFile := path.Join(sourceDirectory, name)
-		destinationFile := path.Join(destinationDirectory, name)
-		
-		if len(specifiedMode) > 0 {
-			copyMode = specifiedMode[0]
-		} else {
-			copyMode = info.Mode()
-		}
-		
-		if info.IsDir() {
-			return CopyDirectory(sourceFile, destinationFile, copyMode)
-		} else {
-			err = CopyFile(sourceFile, destinationFile, copyMode)
-			if err != nil {
-				return err
-			}
+		path1 := path.Join(source, name)
+		path2 := path.Join(destination, name)
+		err = Copy(path1, path2, uid, gid, fileMode, directoryMode)
+		if err != nil {
+			return err
 		}
 	}
-	
 	return nil
+}
+
+func RunScript(content string, environment []string) error {
+	command := exec.Command("sh")
+	command.Env = environment
+	command.Stdin = bytes.NewBufferString(content)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	return command.Run()
 }
